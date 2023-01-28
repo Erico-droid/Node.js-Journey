fdb816258663754cf3d350946521b5787696524f
Eric Kabira <erickabira@gmail.com>
2023-01-28 21:28:59 +0300

    configuring functions

--- Asynchronous Node.js (Weather App)/Weather-app/app.js
+++ Asynchronous Node.js (Weather App)/Weather-app/app.js
@@ -1,22 +1,37 @@
-const { default: axios } = require('axios');
+// const { default: axios } = require('axios');
 const request = require('postman-request');
+const geocode = require('./utils/geocode.js');
+const forecast = require('./utils/forecast.js');
 
-const url =  "http://api.weatherstack.com/current?access_key=7af5817c0a1a17138746d41b4a3c61b9&query=37.8267,-122.4233&units=f";
+// const url =  "http://api.weatherstack.com/current?access_key=7af5817c0a1a17138746d41b4a3c61b9&query=37.8267,-122.4233&units=f";
 
-request({url: url, json: true}, (error, response) => {
-    const data = response.body.current;
-    console.log(`${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`)
-})
+// request({url: url, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('Unable to connect to the weather service!');
+//     } else if (response.body.error) {
+//         console.log("unable to find location");
+//     } else {
+//         const data = response.body.current;
+//     console.log(`${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`)
+//     }
+// })
 
-//Forward Geocoding
-const geocoding_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZXJpY28tZHJvaWQiLCJhIjoiY2xkZzNqdHk2MHR3aTNucTcycDZoY3YyayJ9.Gq-EnuUXhyF0LibM2QQ6uA&limit=1";
-request({url: geocoding_url, json: true}, (error, response) => {
-    const data = response.body.features[0].center
-    const latitude = data[1]
-    const longitude = data[1]
-    console.log(latitude, longitude);
-})
+// //Forward Geocoding
+// const geocoding_url = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZXJpY28tZHJvaWQiLCJhIjoiY2xkZzNqdHk2MHR3aTNucTcycDZoY3YyayJ9.Gq-EnuUXhyF0LibM2QQ6uA&limit=1";
+// request({url: geocoding_url, json: true}, (error, response) => {
+//     if (error) {
+//         console.log('unable to connect to location services');
+//     } else if (!response.body.features.length) {
+//         console.log('We cannot find that location')
+//     } else {
+//     const data = response.body.features[0].center
+//     const latitude = data[1]
+//     const longitude = data[0]
+//     console.log(latitude, longitude);
+// }
+// })
 
+// 
 // trying to make requests using axios
 // async function get_weather_data(url) {
 //    const resp = await axios.get(url)
@@ -33,6 +48,8 @@ request({url: geocoding_url, json: true}, (error, response) => {
 // get_weather_data(url)
 // .then((data) => {
 //     console.log(`${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`)
+// }).catch((err) => {
+//     console.log('something is not right')
 // })
 
 //geocoding using axios and mapbox
@@ -51,3 +68,14 @@ request({url: geocoding_url, json: true}, (error, response) => {
 // .then(() => {
 
 // })
+
+geocode('Nairobi', (error, data) => {
+        // console.log('Error: ', error);
+        // console.log('Data: ', data);
+        // console.log(data.longitude)
+        forecast(data.latitude, data.longitude, (error, data) => {
+            console.log('Error', error)
+            console.log('Data', data)
+          })
+    })
+    
---
+++ Asynchronous Node.js (Weather App)/Weather-app/utils/forecast.js
@@ -0,0 +1,28 @@
+// const request = require('postman-request');
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+const request = require("postman-request");
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=7af5817c0a1a17138746d41b4a3c61b9&query=${latitude},${longitude}&units=m`;
+    request({url: url, json: true}, (error, response) => {
+        if (error) {
+            callback('Unable to connect to the weather service!', undefined)
+        } else if (response.body.error) {
+            callback("unable to find location", undefined)
+        } else {
+            const data = response.body.current;
+            const string = `${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. It feels like ${data.feelslike} degrees out.`
+            callback(undefined, string)
+        }
+    })
+}
+
+module.exports = forecast
